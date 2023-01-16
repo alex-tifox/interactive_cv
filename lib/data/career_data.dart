db3add04ff94fd0f5a824ab4d55ebe5a6daca0ca
Oleksandr Tilnyi <tilnyi.olexander@gmail.com>
2023-01-16 20:06:47 +0100

    Data list filled with new fields

--- lib/data/career_data.dart
+++ lib/data/career_data.dart
@@ -1,70 +1,114 @@
-import 'package:interactive_cv/models/career_model.dart';
+import 'package:interactive_cv/models/experience_model.dart';
 
-abstract class CareerData {
-  static const workExperience = [
+abstract class ExperienceData {
+  static const experience = [
     CareerModel(
       company: 'GlobalLogic Poland (acq. Meelogic Company)',
-      description: 'Some description',
+      position: 'Junior Software Engineer',
+      time: 'Nov 2020 - April 2021',
+      description: 'Worked for lead navigation provider company and on '
+          'in-company tools. Had responsibility for developing new features, '
+          'maintaining existing code base, unit testing, worked with CI/CD '
+          'solutions. Currently gaining experience in Flutter (Method Channels,'
+          ' architecture solutions, unit testing) and Kotlin language '
+          '(Android Auto)',
     ),
     CareerModel(
-      company: 'GlobalLogic Poland (acq. Meelogic Company)',
-      description: 'Some description',
+      company: 'Meelogic Consulting',
+      position: 'Intern Mobile Developer',
+      time: 'Aug 2020 - Sep 2020',
+      description: 'Have been creating in-company realtime application using '
+          'Flutter. Was responsible for whole app development process, except '
+          'backend side. Successful using Dart and Flutter best practices and '
+          'design patterns',
     ),
     CareerModel(
-      company: 'GlobalLogic Poland (acq. Meelogic Company)',
-      description: 'Some description',
+      company: 'Wavery',
+      position: 'Mobile & Backend Engineer',
+      time: 'Jun 2020 - Aug 2021',
+      description: 'Developed a solution of a start-up idea as main developer'
+          ' of mobile application in Flutter and serverless backend'
+          ' using Firebase',
     ),
     CareerModel(
-      company: 'GlobalLogic Poland (acq. Meelogic Company)',
-      description: 'Some description',
+      company: 'Nordic Consulting & Development Company',
+      position: 'Junior Software Engineer',
+      time: 'Dec 2018 - Aug 2019',
+      description:
+          'Worked with TIA-solution based software in insurance industry',
+    ),
+    EducationModel(
+      institution: 'West Pomeranian Technology University in Szczecin',
+      position: 'Bachelor of Science',
+      time: 'Oct 2017 - Mar 2021',
+      description:
+          '- Thesis - \'Using Flutter Framework To Build Simple Car Diagnostic '
+          'Mobile Application Based On OBD-II interface\'. Obtained mark '
+          'for thesis - 5. Average mark obtained - 4',
     ),
     CareerModel(
-      company: 'GlobalLogic Poland (acq. Meelogic Company)',
-      description: 'Some description',
+      company: 'Nordic Consulting & Development Company',
+      position: 'Junior Software Engineer',
+      time: 'Dec 2018 - Aug 2019',
+      description:
+          'Worked with TIA-solution based software in insurance industry',
+    ),
+    EducationModel(
+      institution: 'West Pomeranian Technology University in Szczecin',
+      position: 'Bachelor of Science',
+      time: 'Oct 2017 - Mar 2021',
+      description:
+          '- Thesis - \'Using Flutter Framework To Build Simple Car Diagnostic '
+          'Mobile Application Based On OBD-II interface\'. Obtained mark '
+          'for thesis - 5. Average mark obtained - 4',
     ),
     CareerModel(
-      company: 'GlobalLogic Poland (acq. Meelogic Company)',
-      description: 'Some description',
+      company: 'Nordic Consulting & Development Company',
+      position: 'Junior Software Engineer',
+      time: 'Dec 2018 - Aug 2019',
+      description:
+          'Worked with TIA-solution based software in insurance industry',
+    ),
+    EducationModel(
+      institution: 'West Pomeranian Technology University in Szczecin',
+      position: 'Bachelor of Science',
+      time: 'Oct 2017 - Mar 2021',
+      description:
+          '- Thesis - \'Using Flutter Framework To Build Simple Car Diagnostic '
+          'Mobile Application Based On OBD-II interface\'. Obtained mark '
+          'for thesis - 5. Average mark obtained - 4',
     ),
     CareerModel(
-      company: 'GlobalLogic Poland (acq. Meelogic Company)',
-      description: 'Some description',
+      company: 'Nordic Consulting & Development Company',
+      position: 'Junior Software Engineer',
+      time: 'Dec 2018 - Aug 2019',
+      description:
+          'Worked with TIA-solution based software in insurance industry',
+    ),
+    EducationModel(
+      institution: 'West Pomeranian Technology University in Szczecin',
+      position: 'Bachelor of Science',
+      time: 'Oct 2017 - Mar 2021',
+      description:
+          '- Thesis - \'Using Flutter Framework To Build Simple Car Diagnostic '
+          'Mobile Application Based On OBD-II interface\'. Obtained mark '
+          'for thesis - 5. Average mark obtained - 4',
     ),
     CareerModel(
-      company: 'GlobalLogic Poland (acq. Meelogic Company)',
-      description: 'Some description',
+      company: 'Nordic Consulting & Development Company',
+      position: 'Junior Software Engineer',
+      time: 'Dec 2018 - Aug 2019',
+      description:
+          'Worked with TIA-solution based software in insurance industry',
     ),
-    CareerModel(
-      company: 'GlobalLogic Poland (acq. Meelogic Company)',
-      description: 'Some description',
-    ),
-    CareerModel(
-      company: 'GlobalLogic Poland (acq. Meelogic Company)',
-      description: 'Some description',
-    ),
-    CareerModel(
-      company: 'GlobalLogic Poland (acq. Meelogic Company)',
-      description: 'Some description',
-    ),
-    CareerModel(
-      company: 'GlobalLogic Poland (acq. Meelogic Company)',
-      description: 'Some description',
-    ),
-    CareerModel(
-      company: 'GlobalLogic Poland (acq. Meelogic Company)',
-      description: 'Some description',
-    ),
-    CareerModel(
-      company: 'GlobalLogic Poland (acq. Meelogic Company)',
-      description: 'Some description',
-    ),
-    CareerModel(
-      company: 'GlobalLogic Poland (acq. Meelogic Company)',
-      description: 'Some description',
-    ),
-    CareerModel(
-      company: 'GlobalLogic Poland (acq. Meelogic Company)',
-      description: 'Some description',
+    EducationModel(
+      institution: 'West Pomeranian Technology University in Szczecin',
+      position: 'Bachelor of Science',
+      time: 'Oct 2017 - Mar 2021',
+      description:
+          '- Thesis - \'Using Flutter Framework To Build Simple Car Diagnostic '
+          'Mobile Application Based On OBD-II interface\'. Obtained mark '
+          'for thesis - 5. Average mark obtained - 4',
     ),
   ];
 }
